019d8f52a5fb0a2529cc44e0be8004a67dbfa73e
Chesthajain <cheshtajain1712@gmail.com>
2024-12-16 16:46:48 +0530

    element

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,30 @@
+//jsx
+
 import React from 'react';
+
+//element
+
+const element = React.createElement(
+  'div',
+  {id:'login-btn'},
+  'Login'
+)
+
 class App extends React.Component {
   render() {
-      return(
+      return (
+        <>
           <div>
               <h1>{'Welcome to react world'}</h1>
+              {element}
           </div>
+          
+          </>
       );
   }
+  
 }
+
+
 
 export default App;
