7e3167a408046f57b3e318ab3526be30e0dea1f1
Chesthajain <cheshtajain1712@gmail.com>
2024-12-16 16:35:56 +0530

    jsx

---
+++ jsx.js
@@ -0,0 +1,9 @@
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>{'Welcome to react world'}</h1>
+            </div>
+        )
+    }
+}
---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class App extends React.Component {
+  render() {
+      return(
+          <div>
+              <h1>{'Welcome to react world'}</h1>
+          </div>
+      );
+  }
+}
+
+export default App;
